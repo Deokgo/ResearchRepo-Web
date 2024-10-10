b53e69bcf6c44a3399974f9cb5357a827facaed1
ThatOneLeaf <106152167+ThatOneLeaf@users.noreply.github.com>
2024-10-10 15:31:26 +0800

    fixed rendering error
    adding function reference ()=> to functions would prevent invoking during rendering, preventing infinite loop

--- src/components/home.js
+++ src/components/home.js
@@ -8,7 +8,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Navbar from "./navbar";
-import LoginModal from "./loginmodal"
+import LoginModal from "./loginmodal";
 import {
   Box,
   Button,
@@ -401,7 +401,7 @@ const Home = () => {
                 <Button
                   variant='contained'
                   key={"Get Started"}
-                  onClick={setIsLoginModalOpen(true)}
+                  onClick={() => setIsLoginModalOpen(true)}
                   sx={{
                     backgroundColor: "#CA031B",
                     color: "#FFF",
@@ -446,8 +446,11 @@ const Home = () => {
           </Box>
         </Box>
 
-        <LoginModal show={isModalLoginOpen} close={setIsLoginModalOpen(false)}/>
-        
+        <LoginModal
+          show={isModalLoginOpen}
+          close={() => setIsLoginModalOpen(false)}
+        />
+
         {/*Log In Modal*/}
         <Modal open={isModalLoginOpen} onClose={handleCloseLoginModal}>
           <Box sx={modalStyle}>
