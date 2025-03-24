f0618bc637d88a631e137c9b350232a6cb206d6e
ThatOneLeaf <106152167+ThatOneLeaf@users.noreply.github.com>
2025-03-24 07:28:09 +0100

    fixed ProgDash to allow downloads for export

--- src/pages/ProgDash.js
+++ src/pages/ProgDash.js
@@ -100,7 +100,7 @@ const ProgDash = () => {
               top: 0,
               left: 0,
             }}
-            sandbox='allow-scripts allow-same-origin allow-forms allow-popups'
+            sandbox='allow-scripts allow-same-origin allow-forms allow-popups allow-downloads'
             title='Dash App'
           />
         </Box>
